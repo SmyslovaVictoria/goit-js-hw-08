2f7e4d1e8cff2e1631f1057fb81024626b43daa9
Nikita_Hnitetskyi <108988090+SmyslovaVictoria@users.noreply.github.com>
2023-01-30 16:43:42 +0200

    дз2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(e) {
+  console.log(e.seconds);
+  const time = e.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+}
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+const currentTame = localStorage.getItem('videoplayer-current-time');
+const startTame = JSON.parse(currentTame);
+console.log(startTame);
+player
+  .setCurrentTime(currentTame)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
