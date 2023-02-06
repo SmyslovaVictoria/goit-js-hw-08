55cd84ccdb53375a12470f94b6905ffcf0256c0d
Nikita_Hnitetskyi <108988090+SmyslovaVictoria@users.noreply.github.com>
2023-02-06 20:09:24 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,9 +18,15 @@ form.addEventListener(
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  if (e.value === '') {
-    return alert('Please fill in all the fields!');
+
+  const {
+    elements: { message, email }
+  } = e.currentTarget;
+
+  if (message.value === "" || email.value === "") {
+    return alert("Please fill in all the fields!");
   }
+
   e.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
   console.log(elements);
@@ -34,41 +40,3 @@ function autofill() {
     message.value = savedElements.message || '';
   }
 }
-
-// import throttle from 'lodash.throttle';
-// const obj = {};
-
-// const ref = {
-//   form: document.querySelector('.feedback-form'),
-//   textarea: document.querySelector('.feedback-form textarea'),
-//   input: document.querySelector('.feedback-form input'),
-// };
-
-// ref.form.addEventListener('input', throttle(onFormInput, 500));
-// ref.form.addEventListener('submit', onFormSubmit);
-// returnText();
-
-// function onFormInput(e) {
-//   obj[e.target.name] = e.target.value;
-//   localStorage.setItem('feedback-form-state', JSON.stringify(obj));
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-
-//   if (ref.input.value === '' || ref.textarea.value === '') {
-//     return alert('Please fill in all the fields!');
-//   }
-//   e.currentTarget.reset();
-//   localStorage.removeItem('feedback-form-state');
-// }
-
-// function returnText() {
-//   const savedData = localStorage.getItem('feedback-form-state');
-//   const parsedData = JSON.parse(savedData);
-//   if (parsedData) {
-//     ref.input.value = parsedData.email;
-//     ref.textarea.value = parsedData.message;
-//   }
-// }
